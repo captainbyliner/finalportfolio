ca150d17d176ddcda589e9179349bf68cc3ce25f
captainbyliner <mcarthurdonald@gmail.com>
2014-04-16 19:26:30 -0400

    trying to link it all up

--- application.js
+++ application.js
@@ -1,29 +1,11 @@
 $().ready(function() {
     var controller = $.superscrollorama();
-      controller.addTween(
-        '#examples-parallax', 
-        (new TimelineLite()).append([
-        TweenMax.fromTo($('#parallax-it-left'), 1, {
-        css: {
-            top: 200
-        },
-        immediateRender: true
-    }, {
-        css: {
-            top: -600
-        }
-    }),
-        TweenMax.fromTo($('#parallax-it-right'), 1, {
-        css: {
-            top: 500
-        },
-        immediateRender: true
-    }, {
-        css: {
-            top: -1250
-        }
-    })
-        ]), 1000 // scroll duration of tween
-    );
 
+    // individual element tween examples
+                controller.addTween('#fade-it', TweenMax.from( $('#fade-it'), .5, {css:{opacity: 0}}));
+                controller.addTween('#fly-it', TweenMax.from( $('#fly-it'), .25, {css:{right:'1000px'}, ease:Quad.easeInOut}));
+                controller.addTween('#spin-it', TweenMax.from( $('#spin-it'), .25, {css:{opacity:0, rotation: 720}, ease:Quad.easeOut}));
+                controller.addTween('#scale-it', TweenMax.fromTo( $('#scale-it'), .25, {css:{opacity:0, fontSize:'20px'}, immediateRender:true, ease:Quad.easeInOut}, {css:{opacity:1, fontSize:'240px'}, ease:Quad.easeInOut}));
+                controller.addTween('#smush-it', TweenMax.fromTo( $('#smush-it'), .25, {css:{opacity:0, 'letter-spacing':'30px'}, immediateRender:true, ease:Quad.easeInOut}, {css:{opacity:1, 'letter-spacing':'-10px'}, ease:Quad.easeInOut}), 0, 100); // 100 px offset for better timing
+                
 });
